ac7544f4d89e192ae39ff3f3a3603577d074977c
Madhu Guru <madhu@pop-os.localdomain>
2020-10-27 08:48:21 +0530

    Added vundle support and some plugins

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 sudo apt install git vim
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/madhu-GG/vimrc.git madhu-vimrc
 mv ~/.vimrc ~/.vimrc_bkp
-cp madhu-vimrc/vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 vim +PluginInstall +qall
