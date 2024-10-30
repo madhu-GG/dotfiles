3c052507a857c5e92bc2826122f34fc22725c7a9
Madhu G <trufflemadhu@gmail.com>
2024-10-30 16:40:41 +0530

    Add telescope to nvim dotfile

--- nvim/init.lua
+++ nvim/init.lua
@@ -85,6 +85,12 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 -- end of lazy package manager
 
 -- colorscheme
